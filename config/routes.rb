76b505743c8960d23c4caf57e6ccbb9393856cc5
jay park <jaypark@gmail.com>
2016-01-11 21:38:08 +0900

    크롤링 기능 페이지 분리

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :candidates
   resources :histories
+
   get 'display/index'
+  get 'display/crawl'
   root 'display#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
