f317faa6312c3f242a1849b576559e1b0c14f40c
jay park <jaypark@gmail.com>
2016-02-04 08:39:24 +0900

    김명진무소속 이 저장이 안되네??

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -6,7 +6,8 @@ class DisplayController < ApplicationController
     @last_crawled = History.order("created_at").last
 
     # 한시간마다.
-    if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
+    #if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
+    if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 1800
       @res = `bundle exec ruby ./lib/crawl/crawler/candi_list.rb`
       @history = History.new(:rawdata=>@res)
       @history.save
