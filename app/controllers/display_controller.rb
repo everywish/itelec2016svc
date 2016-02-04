b5eec404ef1f96a3970b6cdd9ef0b192e57a3b7f
jay park <jaypark@gmail.com>
2016-02-04 09:40:35 +0900

    김명진무소속 이 저장이 안되네??

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -6,17 +6,15 @@ class DisplayController < ApplicationController
     @last_crawled = History.order("created_at").last
 
     # 한시간마다.
-    if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
+    if true #@last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
       @res = `bundle exec ruby ./lib/crawl/crawler/candi_list.rb`
       @history = History.new(:rawdata=>@res)
       @history.save
 
       JSON.parse(@res).each do |r|
-        r['necid'] = r['id']
-        r.delete('id')
-        necid = r['necid']
+        r['necid'] = r.delete('id')
         Rails.logger.debug r
-        @candidate = Candidate.find_or_initialize_by(necid: necid)
+        @candidate = Candidate.find_or_initialize_by(necid: r['necid'])
         @candidate.update_attributes(r)
         if @candidate.changed?
           @candidate.save!
