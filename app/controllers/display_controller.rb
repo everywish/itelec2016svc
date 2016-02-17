fb1ad288c6778afa907ebfc0d38a9399cfe3eea1
jaypark <jaypark@gmail.com>
2016-02-17 20:22:16 +0900

    candidate historyid

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -7,19 +7,19 @@ class DisplayController < ApplicationController
   def crawl
     @last_crawled = History.order("created_at").last
 
-    # 한시간마다.
+    # 30분마다
     #if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
-    if true#@last_crawled.nil? or (Time.now - @last_crawled.created_at) > 3600
+    if @last_crawled.nil? or (Time.now - @last_crawled.created_at) > 1800
       @res = `bundle exec ruby ./lib/crawl/crawler/candi_list.rb`
       @history = History.new(:rawdata=>@res)
       @history.save
 
       JSON.parse(@res).each do |r|
         r['necid'] = r.delete('id')
+        r['history_id'] = @history.id
         Rails.logger.debug r
         @candidate = Candidate.find_or_initialize_by(necid: r['necid'])
         @candidate.update_attributes(r)
-        @candidate.history_id=@history.id
         if @candidate.changed?
           @candidate.save!
         end
